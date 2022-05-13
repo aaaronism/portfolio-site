c7ba502cfbce676bf4b2649e4b77dba5bd1770d7
aaaronism <aaronlai01@gmail.com>
2022-05-13 02:11:20 -0400

    projects and more content

--- src/script.js
+++ src/script.js
@@ -17,21 +17,28 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Loader
 const manager = new THREE.LoadingManager()
-manager.onStart = function(url, itemsLoaded, itemsTotal) {
-    console.log('hi')
-}
+
+const loadingScreen = document.getElementById('loading-screen')
+
+// manager.onStart = function(url, itemsLoaded, itemsTotal) {
+//     console.log('hi')
+// }
+
 manager.onLoad = function () {
-    console.log('a')
+
+    loadingScreen.classList.add('fade-out')
+    loadingScreen.style.zIndex = "0"
 }
 
 manager.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log('b')
+    loadingScreen
 }
 
-manager.onError = function (url) {
-    console.log('error')
-}
+// manager.onError = function (url) {
+//     console.log('error')
+// }
 
 const loader5 = new THREE.ObjectLoader(manager)
 loader5.load('', function(object) {
@@ -44,6 +51,20 @@ const gltfLoader = new GLTFLoader()
 let mixer = null
 let mixer2 = null
 let mixer3 = null
+let mixer4 = null
+let mixer5 = null
+let mixer6 = null
+let mixer7 = null
+let mixer8 = null
+let mixer9 = null
+let mixer10 = null
+let mixer11 = null
+let mixer12 = null
+let mixer13 = null
+let mixer14 = null
+let mixer15 = null
+let mixer16 = null
+let mixer17 = null
 
 const firstG = gltfLoader.load(
     '/models/yard_grass/scene.gltf',
@@ -75,6 +96,69 @@ const secondG = gltfLoader.load(
     }
 )
 
+const thirdG = gltfLoader.load(
+    '/models/yard_grass/scene.gltf',
+    (gltf) => {
+        mixer4 = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer4.clipAction(gltf.animations[0])
+
+        action.play()
+
+        gltf.scene.scale.set(0.2, 0.2, 0.2)
+        gltf.scene.position.x = 16
+        gltf.scene.position.y = -3.0
+        scene.add(gltf.scene)
+    }
+)
+
+const fourthG = gltfLoader.load(
+    '/models/yard_grass/scene.gltf',
+    (gltf) => {
+        mixer5 = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer5.clipAction(gltf.animations[0])
+
+        action.play()
+
+        gltf.scene.scale.set(0.2, 0.2, 0.2)
+        gltf.scene.position.x = 8
+        gltf.scene.position.y = -3.5
+        gltf.scene.position.z = 2
+        scene.add(gltf.scene)
+    }
+)
+
+const fifthG = gltfLoader.load(
+    '/models/yard_grass/scene.gltf',
+    (gltf) => {
+        mixer6 = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer6.clipAction(gltf.animations[0])
+
+        action.play()
+
+        gltf.scene.scale.set(0.2, 0.2, 0.2)
+        gltf.scene.position.x = 12
+        gltf.scene.position.y = -3.5
+        gltf.scene.position.z = 2
+        scene.add(gltf.scene)
+    }
+)
+
+const sixthG = gltfLoader.load(
+    '/models/yard_grass/scene.gltf',
+    (gltf) => {
+        mixer7 = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer7.clipAction(gltf.animations[0])
+
+        action.play()
+
+        gltf.scene.scale.set(0.2, 0.2, 0.2)
+        gltf.scene.position.x = 16
+        gltf.scene.position.y = -3.5
+        gltf.scene.position.z = 2
+        scene.add(gltf.scene)
+    }
+)
+
 const city = gltfLoader.load(
     '/models/city/scene.gltf',
     (gltf) => {
@@ -84,12 +168,143 @@ const city = gltfLoader.load(
         action.play()
 
         gltf.scene.scale.set(0.0019, 0.0019, 0.0019)
-        gltf.scene.position.x = 18.5
+        gltf.scene.position.x = 22.7
         gltf.scene.position.y = -3
         gltf.scene.position.z = 1
         scene.add(gltf.scene)
     }
 )
+
+const cloud1 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer8 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 3)
+        gltf.scene.position.x = 35
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -2
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud2 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer9 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 3)
+        gltf.scene.position.x = 35
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -2
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud3 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer10 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 2)
+        gltf.scene.position.x = 31
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -5
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud4 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer11 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 2)
+        gltf.scene.position.x = 31
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -5
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud5 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer12 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 2)
+        gltf.scene.position.x = 36
+        gltf.scene.position.y = -1
+        gltf.scene.position.z = -13
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud6 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer13 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(1, 1, 2)
+        gltf.scene.position.x = 36
+        gltf.scene.position.y = -1
+        gltf.scene.position.z = -13
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud7 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer14 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(2, 2, 5)
+        gltf.scene.position.x = 39
+        gltf.scene.position.y = 1
+        gltf.scene.position.z = -18
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud8 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer15 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(2, 2, 5)
+        gltf.scene.position.x = 39
+        gltf.scene.position.y = 1
+        gltf.scene.position.z = -18
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud9 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer16 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(.6, .5, .5)
+        gltf.scene.position.x = 41
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -9
+        scene.add(gltf.scene)
+    }
+)
+
+const cloud10 = gltfLoader.load(
+    '/models/cloud_test/scene.gltf',
+    (gltf) => {
+        mixer17 = new THREE.AnimationMixer(gltf.scene)
+
+        gltf.scene.scale.set(.5, .5, .5)
+        gltf.scene.position.x = 41
+        gltf.scene.position.y = -4
+        gltf.scene.position.z = -9
+        scene.add(gltf.scene)
+    }
+)
+
 
 // Objects
 // Texture
@@ -145,6 +360,12 @@ const sectionMeshes = [ mesh1 ]
 // Text
 const loader = new FontLoader()
 const loader2 = new FontLoader()
+const loader3 = new FontLoader()
+const loader4 = new FontLoader()
+const loader6 = new FontLoader()
+const loader7 = new FontLoader()
+const loader8 = new FontLoader()
+const loader9 = new FontLoader()
 
 loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
 
@@ -181,13 +402,116 @@ mesh5.position.y = .48
 scene.add(mesh5)
 })
 
+
+loader3.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry3 = new TextGeometry("empathetic", {
+    font: font,
+    size: .5,
+    height: .2,
+})
+    
+const mesh6 = new THREE.Mesh(geometry3, material)
+
+mesh6.position.x = 28
+mesh6.position.y = -.80
+mesh6.position.z = -5
+
+scene.add(mesh6)
+})
+
+loader4.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry4 = new TextGeometry("teamplayer", {
+    font: font,
+    size: .5,
+    height: .2,
+})
+    
+const mesh7 = new THREE.Mesh(geometry4, material)
+
+mesh7.position.x = 34
+mesh7.position.y = -.18
+mesh7.position.z = -7
+
+scene.add(mesh7)
+})
+
+loader6.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry5 = new TextGeometry("hardworking", {
+    font: font,
+    size: .5,
+    height: .2,
+})
+    
+const mesh8 = new THREE.Mesh(geometry5, material)
+
+mesh8.position.x = 33
+mesh8.position.y = 3
+mesh8.position.z = -6
+
+scene.add(mesh8)
+})
+
+loader7.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry6 = new TextGeometry("curious", {
+    font: font,
+    size: .7,
+    height: .2,
+})
+    
+const mesh9 = new THREE.Mesh(geometry6, material)
+
+mesh9.position.x = 36
+mesh9.position.y = -1.5
+mesh9.position.z = -1
+
+scene.add(mesh9)
+})
+
+loader8.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry7 = new TextGeometry("patient", {
+    font: font,
+    size: .5,
+    height: .2,
+})
+    
+const mesh10 = new THREE.Mesh(geometry7, material)
+
+mesh10.position.x = 33
+mesh10.position.y = 2
+mesh10.position.z = -12
+
+scene.add(mesh10)
+})
+
+loader9.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/fonts/droid/droid_sans_bold.typeface.json', function (font) {
+
+    const geometry8 = new TextGeometry("open-minded", {
+    font: font,
+    size: .2,
+    height: .2,
+})
+    
+const mesh11 = new THREE.Mesh(geometry8, material)
+
+mesh11.position.x = 39
+mesh11.position.y = .5
+mesh11.position.z = -6
+
+scene.add(mesh11)
+})
+
 // Particles
 // Geometry
-const particlesCount = 1000
-const positions = new Float32Array(particlesCount * 3)
+const particlesCount = 3000
+const positions = new Float32Array(particlesCount * 5)
 
 for(let i = 0; i < particlesCount; i++) {
-    positions[i * 3 + 0] = (Math.random() - 0.5) * 50
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 90
     positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
     positions[i * 3 + 2] = (Math.random() - 0.2) * 100
 }
@@ -370,10 +694,10 @@ document.addEventListener("scroll", () => {
     let y = window.scrollY
     let one = Math.round(scrollY / sizes.width)
     let two = scrollY / sizes.width
-    if (two != .67 && two < .67) {
+    if (two != .32 && two < .32) {
         document.getElementById('aboutme').style.opacity = "0"
         document.getElementById('aboutme').style.animation = "none"
-    } else if (two >= .67 && two < 1.3) {
+    } else if (two >= .32 && two < 0.82) {
         showAbout()
     } else {
         document.getElementById('aboutme').style.opacity = "0"
@@ -382,15 +706,21 @@ document.addEventListener("scroll", () => {
     //     document.getElementById('aboutme').style.opacity = "0"
     //     document.getElementById('aboutme').style.animation = "fadeOut 1s forwards"
     }
-    if (two != 1.5 && two < 1.5) {
+    if (two != 0.91 && two < 0.91) {
         document.getElementById('projectbox').style.opacity = "0"
         document.getElementById('projectbox').style.animation = "none"
-    } else if (two >= 1.5 && two < 2.2) {
+        document.getElementById('projectitle').style.opacity = "0"
+        document.getElementById('projectitle').style.animation = "none"
+    } else if (two >= 0.91 && two < 1.28) {
         document.getElementById('projectbox').style.opacity = "1"
         document.getElementById('projectbox').style.animation = "fadeIn 1s forwards"
+        document.getElementById('projectitle').style.opacity = "1"
+        document.getElementById('projectitle').style.animation = "fadeIn 3s forwards"
     } else {
         document.getElementById('projectbox').style.opacity = "0"
         document.getElementById('projectbox').style.animation = "fadeOut 1s forwards"
+        document.getElementById('projectitle').style.opacity = "0"
+        document.getElementById('projectitle').style.animation = "fadeOut 1s forwards"
     // } else if (one < 1) {
     //     document.getElementById('aboutme').style.opacity = "0"
     //     document.getElementById('aboutme').style.animation = "fadeOut 1s forwards"
@@ -421,12 +751,27 @@ const tick = () =>
         if(mixer !== null) {
             mixer.update(.002)
             }
-            if(mixer2 !== null) {
-                mixer2.update(.002)
-                }
+        if(mixer2 !== null) {
+            mixer2.update(.002)
+            }
+        if(mixer3 !== null) {
+            mixer3.update(.01)
+            }
+        if(mixer4 !== null) {
+            mixer4.update(.002)
+            }
+        if(mixer5 !== null) {
+            mixer5.update(.002)
+            }
+        if(mixer6 !== null) {
+            mixer6.update(.002)
+            }
+        if(mixer7 !== null) {
+            mixer7.update(.002)
+            }
         
             // animate camera
-            camera.position.x = scrollY / sizes.width * 10
+            camera.position.x = scrollY / sizes.width * 20
             // if (camera.position.x > 20) {
             //     camera.position.z = ((scrollY+1) - scrollY) 
             // } else {
